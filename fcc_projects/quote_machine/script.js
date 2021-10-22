24aeecea0a0408c1fb3ef06a5faad2e5737c1c7e
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-10-22 14:27:14 +0200

    Update script.js

--- fcc_projects/quote_machine/script.js
+++ fcc_projects/quote_machine/script.js
@@ -1,5 +1,14 @@
 document.getElementById("new-quote").addEventListener("click", getQuote);
 
 function getQuote() {
- document.getElementById("text").innerHTML = "Hello World!";
+  fetch("https://animechan.vercel.app/api/random")
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                console.log(data.anime)
+                document.getElementById('author').innerText = data.character;
+                document.getElementById("text").innerHTML = data.quote;
+                document.getElementById("anime").innerHTML = data.anime;
+            });
 }
