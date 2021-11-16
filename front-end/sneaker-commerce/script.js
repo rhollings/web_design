ee6b992bbca0ba1feb53327fa95b88737f6426c5
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-11-16 21:22:09 +0100

    Add files via upload

---
+++ front-end/sneaker-commerce/script.js
@@ -0,0 +1,15 @@
+function changeOne() {
+    document.getElementById('main-img').src = 'images/image-product-1.jpg'
+}
+
+// How  '-' and '+' work
+var count = 0;
+function addOne() {
+    updateCount(++count);
+}
+function subOne() {
+    updateCount(--count);
+}
+function updateCount(val) {
+    document.getElementById("quantity").innerHTML = val;
+}
